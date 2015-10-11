da4172fefd1df3ad83f4ad6c3d584bcd3c0c347c
Danny Weeks <danny@dannyweeks.com>
2015-10-12 00:01:19 +0100

    update location of example server.json to copy from

--- init.sh
+++ init.sh
@@ -2,6 +2,6 @@
 
 mkdir -p ~/.mersey
 
-cp -i src/servers.json.example ~/.mersey/servers.json
+cp -i ~/.composer/vendor/dannyweeks/mersey/servers.json.example ~/.mersey/servers.json
 
 echo "Mersey is ready to rock! Add your servers in ~/.mersey/servers.json"
